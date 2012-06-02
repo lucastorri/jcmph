3b7afdc01795e7b460ccae07ad04270ac5ad0d5f
Lucas Torri <lucastorri@gmail.com>
2012-06-02 19:56:05 -0300

    first commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+organization := "co.torri"
+
+name := "jcmcph"
+
+version := "1.0"
+
+scalaVersion := "2.9.2"
+
+scalacOptions += "-deprecation"
+
+scalacOptions += "-unchecked"
+
---
+++ src/main/scala/test.scala
@@ -0,0 +1,14 @@
+package main.scala
+
+object Main {
+
+  System.loadLibrary("cmph")
+
+  def main(args: Array[String]) = {
+  
+    println("hello")
+  }
+
+}
+
+// vim: set ts=4 sw=4 et:
